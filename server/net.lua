0baf71957f21e8fddc66b4a1d0a3b2683d8a6288
Brendon_Smith <81374445+LOTW1869@users.noreply.github.com>
2025-01-26 11:04:51 +0800

    Update net.lua (#7)
    fix net
    
    Co-authored-by: Brendon_Smith <81374445+RDRRoleplay1869@users.noreply.github.com>

--- server/net.lua
+++ server/net.lua
@@ -1,4 +1,3 @@
-
 if Config.Net.active then
     local players = {}
 
@@ -9,30 +8,38 @@ if Config.Net.active then
 
     Citizen.CreateThread(function()
         while true do
+
             for k, v in ipairs(GetPlayers()) do
                 if GetPlayerPed(v) ~= 0 then
-                    if players[v] then
-                        if players[v].callback == false then
-                            Discord.sendMessage(v, Config.Discord.lang.kick .. Config.Net.lang.kickreason)
-                            DropPlayer(v, Config.Discord.lang.kick .. Config.Net.lang.kickreason)
-                        else
-                            players[v].callback = false
-                            TriggerClientEvent('heartbeat:Client', v)
-                        end
-                    else
+
+                    if not players[v] then
                         players[v] = {name = GetPlayerName(v), id = v, callback = nil}
+                    end
+
+
+                    if players[v].callback == false then
+                        Discord.sendMessage(v, Config.Discord.lang.kick .. Config.Net.lang.kickreason)
+                        DropPlayer(v, Config.Discord.lang.kick .. Config.Net.lang.kickreason)
+                    else
+ 
+                        players[v].callback = false
+
+                        TriggerClientEvent('heartbeat:Client', v)
                     end
                 end
             end
+
             Citizen.Wait(Config.Net.checkinrate)
         end
     end)
+
 
     RegisterServerEvent('heartbeat:Server')
     AddEventHandler('heartbeat:Server', function()
         local _source = source
         local v = tostring(_source)
         if players[v] then
+
             players[v].callback = true
         else
             print('No user on hearbeat')
